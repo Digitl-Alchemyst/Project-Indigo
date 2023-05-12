69465df0f5a416ee3b9b867fe93234abe4e00340
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-05-12 15:33:30 -0500

    added file for api management

---
+++ client/src/state/api.js
@@ -0,0 +1,6 @@
+import { createAPI , fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const api = createAPI({
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+
+});
