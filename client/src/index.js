b35c20eec60b1a06700e9c1f5b058b664738a176
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-05-16 22:08:47 -0500

    .

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { configureStore } from '@reduxjs/toolkit';
-import globalReducer from 'state';
-import { Provider } from 'react-redux';
-import { setupListeners } from '@reduxjs/toolkit/query';
-import { api } from 'state/api';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "state";
+import { Provider } from "react-redux";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "state/api";
 
 const store = configureStore({
   reducer: {
@@ -15,7 +15,6 @@ const store = configureStore({
   },
   middleware: (getDefault) => getDefault().concat(api.middleware),
 });
-
 setupListeners(store.dispatch);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -26,4 +25,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
