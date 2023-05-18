6391dbfefc16e1c90567d43e997fb8c54b7a19bf
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-05-18 17:22:14 -0500

    Added Products Page Biolerplate

---
+++ client/src/scenes/Products/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  Box,
+  Card,
+  CardActions,
+  Card,
+  Content,
+  Collapse,
+  Button,
+  Typography,
+  Rating,
+  IconButton,
+} from "@material-ui/core";
+
+function Products() {
+  return <div>Products</div>;
+}
+
+export default Products;
