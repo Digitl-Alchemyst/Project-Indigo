04ae5c3a232e9290411ea881edc005028fa661a0
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-05-18 21:32:43 -0500

    fixed folder capitalization

---
+++ client/src/scenes/products/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Collapse,
+  Button,
+  Typography,
+  Rating,
+  IconButton,
+} from "@mui/material";
+
+function Products() {
+  return <div>Products</div>;
+}
+
+export default Products;
