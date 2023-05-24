772768df9fa12f07a720d0d94c8d2e6b79a78ba0
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-05-23 23:32:00 -0500

    Transactions API Query Transactions Page Boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Dashboard from './scenes/dashboard';
 import Layout from './scenes/layout';
 import Products from "scenes/products";
 import Customers from "./scenes/customers";
-// import Transactions from "scenes/transactions";
+import Transactions from "scenes/transactions";
 // import Geography from "scenes/geography";
 // import Overview from "scenes/overview";
 // import Daily from "scenes/daily";
@@ -35,8 +35,8 @@ function App() {
               <Route path="/form" element={<Form />} />
               {/* <Route path="/Clients" element={<Clients />} /> */}
               <Route path="/customers" element={<Customers />} />
-              {/* <Route path="/transactions" element={<Transactions />} />
-              <Route path="/geography" element={<Geography />} />
+              <Route path="/transactions" element={<Transactions />} />
+              {/* <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
               <Route path="/monthly" element={<Monthly />} />
