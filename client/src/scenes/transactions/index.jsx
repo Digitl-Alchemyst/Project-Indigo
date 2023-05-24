014e87fc4950019c342ee8e3b05482bacd43cb75
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-05-23 23:32:00 -0500

    Transactions API Query Transactions Page Boilerplate

---
+++ client/src/scenes/transactions/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import Header from 'components/Header'
+
+const Transactions = () => {
+  return (
+    <Box>
+        <Header title="Transactions" subtitle="Transactions Overview" />
+    </Box>
+    
+  )
+}
+
+export default Transactions
