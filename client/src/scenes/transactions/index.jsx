dd5ae5cff6657e7fb2b742f794ce56faedd9a615
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-05-23 23:52:04 -0500

    Query Params set up for Transaction page

--- client/src/scenes/transactions/index.jsx
+++ client/src/scenes/transactions/index.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useState } from 'react'
+import { dataGrid } from '@mui/x-data-grid'
+import { useGetTransactionsQuery } from 'state/api'
 import { Box } from '@mui/material'
 import Header from 'components/Header'
+import { useTheme } from '@mui/material/styles'
+import theme from 'theme'
 
 const Transactions = () => {
+    // const useTheme = useTheme()
+
+    // Values to be sent to back-end
+    const [page, setPage] = useState(0);
+    const [pageSize, setPageSize] = useState(20);
+    const [sort, setSort] = useState({});
+    const [search, setSearch] = useState("");
+
+    const { data, isLoading } = useGetTransactionsQuery({
+        page,
+        pageSize,
+        sort: JSON.stringify(sort),
+        search,
+    });
+    console.log("data:", data)
+
   return (
     <Box>
         <Header title="Transactions" subtitle="Transactions Overview" />
