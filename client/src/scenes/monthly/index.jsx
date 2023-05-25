4c64a371935c1a331f62ddfcca23411b09dff15e
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-05-25 15:44:54 -0500

    Styling tweeks

---
+++ client/src/scenes/monthly/index.jsx
@@ -0,0 +1,178 @@
+import React, { useMemo } from "react";
+import { Box, useTheme } from "@mui/material";
+import Header from "components/Header";
+import { useGetSalesQuery } from "state/api";
+import Loading from "components/Loading";
+import { ResponsiveLine } from "@nivo/line";
+
+const Monthly = () => {
+  const theme = useTheme();
+
+  const { data } = useGetSalesQuery();
+
+  const [formattedData] = useMemo(() => {
+    if (!data) return [];
+
+    const { monthlyData } = data;
+    const totalSalesLine = {
+      id: "Total Sales",
+      color: theme.palette.secondary[200],
+      data: [],
+    };
+    const totalUnitsLine = {
+      id: "Total Units",
+      color: theme.palette.secondary[600],
+      data: [],
+    };
+
+    Object.values(monthlyData).forEach(({ month, totalSales, totalUnits }) => {
+      totalSalesLine.data = [
+        ...totalSalesLine.data,
+        { x: month, y: totalSales },
+      ];
+      totalUnitsLine.data = [
+        ...totalUnitsLine.data,
+        { x: month, y: totalUnits },
+      ];
+    });
+
+    const formattedData = [totalSalesLine, totalUnitsLine];
+
+    return [formattedData];
+  }, [data]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="Monthly Sales" subtitle="Monthly Sales Overview" />
+
+      <Box
+        sx={{
+          // display: "flex",
+          // justifyContent: "center",
+          // alignItems: "center",
+          height: "80vh",
+          width: "86.5vw",
+          mt: "5px",
+          ml: "10px",
+          pb: "20px",
+          backgroundColor: theme.palette.primary[900],
+          border: "1px solid",
+          borderColor: theme.palette.primary[400],
+          borderRadius: "4px",
+          boxShadow: theme.shadows[3],
+        }}
+      >
+        {data ? (
+          <ResponsiveLine
+            data={formattedData}
+            theme={{
+              axis: {
+                domain: {
+                  line: {
+                    stroke: theme.palette.secondary[200],
+                  },
+                },
+                legend: {
+                  text: {
+                    fill: theme.palette.secondary[200],
+                  },
+                },
+                ticks: {
+                  line: {
+                    stroke: theme.palette.secondary[200],
+                    strokeWidth: 1,
+                  },
+                  text: {
+                    fill: theme.palette.secondary[200],
+                  },
+                },
+              },
+              legends: {
+                text: {
+                  fill: theme.palette.secondary[200],
+                },
+              },
+              tooltip: {
+                container: {
+                  background: theme.palette.primary[200],
+                  color: theme.palette.secondary[500],
+                },
+              },
+            }}
+            colors={{ datum: "color" }}
+            margin={{ top: 20, right: 50, bottom: 70, left: 80 }}
+            xScale={{ type: "point" }}
+            yScale={{
+              type: "linear",
+              min: "auto",
+              max: "auto",
+              stacked: false,
+              reverse: false,
+            }}
+            yFormat=" >-.2f"
+            curve="catmullRom"
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+              orient: "bottom",
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 90,
+              legend: "Month",
+              legendOffset: 60,
+              legendPosition: "middle",
+            }}
+            axisLeft={{
+              orient: "left",
+              tickValues: 5,
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: "Total",
+              legendOffset: -60,
+              legendPosition: "middle",
+            }}
+            enableGridX={false}
+            enableGridY={true}
+            pointSize={10}
+            pointColor={{ theme: "background" }}
+            pointBorderWidth={2}
+            pointBorderColor={{ from: "serieColor" }}
+            pointLabelYOffset={-12}
+            useMesh={true}
+            legends={[
+              {
+                anchor: "top-right",
+                direction: "column",
+                justify: false,
+                translateX: 30,
+                translateY: -10,
+                itemsSpacing: 0,
+                itemDirection: "left-to-right",
+                itemWidth: 100,
+                itemHeight: 20,
+                itemOpacity: 0.85,
+                symbolSize: 18,
+                symbolShape: "circle",
+                symbolBorderColor: "rgba(0, 0, 0, .5)",
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemBackground: "rgba(0, 0, 0, .03)",
+                      itemOpacity: 1,
+                    },
+                  },
+                ],
+              },
+            ]}
+          />
+        ) : (
+          <Loading />
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default Monthly;
