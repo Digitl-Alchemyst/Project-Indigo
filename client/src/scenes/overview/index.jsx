0febe43b414fb3adcb2417498ef924368a06261f
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-05-25 13:09:09 -0500

    Overview Page Complete with Bug

--- client/src/scenes/overview/index.jsx
+++ client/src/scenes/overview/index.jsx
@@ -1,68 +1,117 @@
-import React from 'react'
-import { Box, useTheme, Button } from '@mui/material'
-import Header from 'components/Header.jsx'
-
-
+import React, { useState } from "react";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Box,
+  Button,
+  useTheme,
+} from "@mui/material";
+import Header from "components/Header.jsx";
+import OverviewChart from "components/OverviewChart.jsx";
+import FlexBetween from "components/FlexBetween";
 
 const Overview = () => {
-    const theme = useTheme();
+  const theme = useTheme();
+
+  const [view, setView] = useState("units");
 
   return (
-
     <Box m="1.5rem 2.5rem">
-        <Header title="Sales Overview" subtitle="Overview of Recent Sales" />
+      <Header title="Sales Overview" subtitle="Overview of Recent Sales" />
 
-        <Box 
-            sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '79vh',
-                width: '86.5vw',
-                mt: '20px',
-                ml: '10px',
-                backgroundColor: theme.palette.primary[900],
-                border: '1px solid',                
-                borderColor: theme.palette.primary[400],
-                borderRadius: '4px',
-                boxShadow: theme.shadows[3],
-            }}
+      <FormControl sx={{ mt: "1rem", ml: "10px" }}>
+        <InputLabel>View</InputLabel>
+        <Select
+          value={view}
+          label="View"
+          onChange={(e) => setView(e.target.value)}
         >
+          <MenuItem value="units">Units</MenuItem>
+          <MenuItem value="revenue">Sales</MenuItem>
+        </Select>
+      </FormControl>
 
-            
+      <Box
+        sx={{
+          height: "73vh",
+          width: "86.5vw",
+          mt: "20px",
+          ml: "10px",
+          mb: "5px",
+          backgroundColor: theme.palette.primary[800],
+          border: "1px solid",
+          borderColor: theme.palette.primary[400],
+          borderRadius: "4px",
+          boxShadow: theme.shadows[3],
+        }}
+      >
+        {/* <OverviewChart view={view} /> */}
+        <OverviewChart view={view} />
+      </Box>
 
-
-
-        </Box>
-
-        <Button sx={{
-            height: '50px',
-            width: '150px',
-            backgroundColor: theme.palette.primary[900],
-            color: theme.palette.secondary[200],
-            border: '1px solid',
-            borderColor: theme.palette.primary[400],
-            borderRadius: '4px',
-            boxShadow: theme.shadows[3],
-            textAlign: 'center',
-            fontSize: '0.75rem',
-            fontWeight: 'bold',
-            ml: '10px',
-            mt: '5px',
-            '&:hover': {
+      <Box>
+        <FlexBetween>
+          <Button
+            sx={{
+              height: "50px",
+              width: "150px",
+              anchor: "left",
+              backgroundColor: theme.palette.primary[900],
+              color: theme.palette.secondary[200],
+              border: "1px solid",
+              borderColor: theme.palette.primary[400],
+              borderRadius: "4px",
+              boxShadow: theme.shadows[3],
+              textAlign: "center",
+              fontSize: "0.75rem",
+              fontWeight: "bold",
+              ml: "10px",
+              mt: "5px",
+              "&:hover": {
                 backgroundColor: theme.palette.primary[700],
                 color: theme.palette.secondary[300],
-                border: '1px solid',
+                border: "1px solid",
                 borderColor: theme.palette.primary[300],
-                borderRadius: '4px',
-            }
-        }}
-        >
+                borderRadius: "4px",
+              },
+            }}
+          >
             Download Report
-        </Button>
+          </Button>
 
+          <Button
+            sx={{
+              height: "50px",
+              width: "150px",
+              anchor: "right",
+              backgroundColor: theme.palette.primary[900],
+              color: theme.palette.secondary[200],
+              border: "1px solid",
+              borderColor: theme.palette.primary[400],
+              borderRadius: "4px",
+              boxShadow: theme.shadows[3],
+              textAlign: "center",
+              fontSize: "0.75rem",
+              fontWeight: "bold",
+              mr: "10px",
+              mt: "5px",
+              "&:hover": {
+                backgroundColor: theme.palette.primary[700],
+                color: theme.palette.secondary[300],
+                border: "1px solid",
+                borderColor: theme.palette.primary[300],
+                borderRadius: "4px",
+              },
+            }}
+          >
+            Email CVS
+          </Button>
+        </FlexBetween>
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default Overview
+export default Overview;
