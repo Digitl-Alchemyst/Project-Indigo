2f6b70dd472dd0d0a9ba2b0d3e78f54803654f40
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-05-25 15:44:54 -0500

    Styling tweeks

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import Geography from "scenes/geography";
 import Transactions from "scenes/transactions";
 import Overview from "scenes/overview";
 import Daily from "scenes/Daily";
-// import Monthly from "scenes/monthly";
+import Monthly from "scenes/monthly";
 // import Breakdown from "scenes/breakdown";
 // import Admin from "scenes/admin";
 // import Performance from "scenes/performance";
@@ -39,7 +39,7 @@ function App() {
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
-              {/* <Route path="/monthly" element={<Monthly />} /> */}
+              <Route path="/monthly" element={<Monthly />} />
               {/* <Route path="/breakdown" element={<Breakdown />} /> */}
               {/* <Route path="/admin" element={<Admin />} /> */}
               {/* <Route path="/performance" element={<Performance />} />   */}
